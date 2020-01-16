586547879d5d2f77e8135ad00d136a24c17e49a0
miguel <climaxfran@hotamil.com>
2020-01-16 13:29:25 -0400

    aprendiendo de clases

--- py_proyect/piesas/piesas_2.py
+++ py_proyect/piesas/piesas_2.py
@@ -1,2 +1,9 @@
 from playsaund import playsaund
 playsaund("twenty-one-pilots-nico-and-the-niners-official-video")
+
+class Myclass(object):
+    
+     i = 123
+
+    def say_hello(self):
+        return 'Hi there!'
